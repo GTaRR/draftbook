2b4313082b102cdfc3774f9ce95447eb734aa1b7
GTaRR <zakolbos@mail.ru>
2019-08-11 21:13:37 +0300

    Init

---
+++ src/components/Editor.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ckeditor :editor="editor" :value="editorData" :config="editorConfig"></ckeditor>
+  </div>
+</template>
+
+<script>
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import '@ckeditor/ckeditor5-build-classic/build/translations/ru';
+
+export default {
+  name: 'Editor',
+  props: [
+    'editorData'
+  ],
+  data() {
+      return {
+          editor: ClassicEditor,
+          // editorData: '',
+          editorConfig: {
+              language: 'ru'
+          }
+      };
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
