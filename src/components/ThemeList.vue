94d02993f3eadadc1a92ecc03f61f47b6eb0df3d
GTaRR <zakolbos@mail.ru>
2020-05-14 09:07:21 +0300

    Decomposition project

---
+++ src/components/ThemeList.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="theme-list">
+    <div
+      class="theme-item default"
+      @click="changeTheme('default')"
+      :class="{active: theme === 'default'}"
+    >
+      <div class="theme-sidebar">
+        <div class="theme-sidebar__top"></div>
+        <div class="theme-sidebar__bottom"></div>
+      </div>
+      <div class="theme-body"></div>
+    </div>
+
+    <div
+      class="theme-item colored"
+      @click="changeTheme('colored')"
+      :class="{active: theme === 'colored'}"
+    >
+      <div class="theme-sidebar">
+        <div class="theme-sidebar__top"></div>
+        <div class="theme-sidebar__bottom"></div>
+      </div>
+      <div class="theme-body"></div>
+    </div>
+
+    <div
+      class="theme-item dark"
+      @click="changeTheme('dark')"
+      :class="{active: theme === 'dark'}"
+    >
+      <div class="theme-sidebar">
+        <div class="theme-sidebar__top"></div>
+        <div class="theme-sidebar__bottom"></div>
+      </div>
+      <div class="theme-body"></div>
+    </div>
+
+    <div
+      class="theme-item colored dark"
+      @click="changeTheme('colored-dark')"
+      :class="{active: theme === 'colored-dark'}"
+    >
+      <div class="theme-sidebar">
+        <div class="theme-sidebar__top"></div>
+        <div class="theme-sidebar__bottom"></div>
+      </div>
+      <div class="theme-body"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters([
+      'color',
+      'darkMode',
+      'coloredMode',
+      'customColor',
+      'theme',
+      'themes'
+    ])
+  }
+}
+</script>
