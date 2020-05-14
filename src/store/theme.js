a7785782175c7da7591e54423d952555d40cc658
Vladimir Sushilov <33573215+GTaRR@users.noreply.github.com>
2020-05-14 09:11:42 +0300

    Decomposition project

--- src/store/theme.js
+++ src/store/theme.js
@@ -1,14 +1,119 @@
 export default {
   store: {
-
+    color: {
+      hue: 163,
+      saturation: 38,
+      luminosity: 62,
+      alpha: 1
+    },
+    darkMode: false,
+    coloredMode: false,
+    customColor: false,
+    theme: 'default',
+    themes: [
+      {
+        name: 'default',
+        saturation: 38,
+        luminosity: 62,
+        background: '#fff',
+        sidebarBackground: '#fff',
+        light: '#f8f9fa',
+        lightHover: '#eee',
+        lightActive: '#eee',
+        gray: {
+          400: "#ced4da",
+          500: "#aaa",
+          700: "#5a5a5a",
+          800: "#343a40",
+        },
+        dark: false,
+        colored: false,
+      },
+      {
+        name: 'dark',
+        saturation: 25,
+        luminosity: 48,
+        background: '#202020',
+        sidebarBackground: '#202020',
+        light: '#252424',
+        lightHover: '#2d2d2d',
+        lightActive: '#1b1b1b',
+        gray: {
+          400: "#45403c",
+          500: "#606060",
+          700: "#ccc",
+          800: "#ccc",
+        },
+        dark: true,
+        colored: false,
+      },
+      {
+        name: 'colored',
+        saturation: 36,
+        luminosity: 62,
+        background: '#fff',
+        sidebarBackground: '#fff',
+        light: '#f8f9fa',
+        lightHover: '#eee',
+        lightActive: '#eee',
+        gray: {
+          400: "#ced4da",
+          500: "#aaa",
+          700: "#5a5a5a",
+          800: "#343a40",
+        },
+        dark: false,
+        colored: true,
+      },
+      {
+        name: 'colored-dark',
+        saturation: 32,
+        luminosity: 54,
+        background: '#202020',
+        sidebarBackground: '#202020',
+        light: '#252424',
+        lightHover: '#2d2d2d',
+        lightActive: '#393334',
+        gray: {
+          400: "#45403c",
+          500: "#606060",
+          700: "#ccc",
+          800: "#ccc",
+        },
+        dark: true,
+        colored: true,
+      },
+    ]
   },
   getters: {
-
+    color: s => s.color,
+    darkMode: s => s.darkMode,
+    coloredMode: s => s.coloredMode,
+    customColor: s => s.customColor,
+    theme: s => s.theme,
+    themes: s => s.themes
   },
   actions: {
-
+    changeColor({commit, getters}, hue) {
+      commit('changeColor', hue);
+      commit('changeTheme', getters.theme);
+    },
+    changeTheme({commit}, name) {
+      commit('changeTheme', name)
+    }
   },
   mutations: {
+    changeColor(state, hue) {
+      state.color.hue = hue;
+      state.customColor = true;
+      state.changeTheme(state.theme);
+    },
+    changeTheme(state, name) {
+      state.theme = name;
+      const theme = state.themes.filter(theme => theme.name === name);
 
+      state.darkMode = theme.dark;
+      state.coloredMode = theme.colored;
+    }
   }
 }
